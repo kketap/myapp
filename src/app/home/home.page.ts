7c04607363e713e3aa5f3303c3ab7c0ad8fa746b
kketap <ddiego.alonso.bravo@gmail.com>
2023-08-30 15:16:31 -0400

    hola proyecto

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router';
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor(private router: Router) { }
+
+  public mensaje = ""
+
+  user = {
+    usuario: "",
+    password: ""
+  }
+
+
+  enviarInformacion() {
+    let navigationExtras: NavigationExtras = {
+      state: { user: this.user }
+    }
+    this.router.navigate(['/login'], navigationExtras);
+  }
+  mostrarConsola() {
+    console.log(this.user);
+    if (this.user.usuario != "" && this.user.password != "") {
+      this.mensaje = "Usuario Conectado";
+    } else {
+      this.mensaje = "Usuario y contraseña deben tener algun valor"
+    }
+  }
+
+}
